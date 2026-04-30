04a8302ef146de7ad178624f9818f98ff9cd05ba
Zetaku1 <jpbalazs98@gmail.com>
2026-04-30 17:12:00 -0400

    Initial commit: Beatfigther empty ruleset

---
+++ osu.Game.Rulesets.BeatFighter/Replays/BeatFighterReplayFrame.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using System.Linq;
+using osu.Game.Rulesets.Replays;
+using osuTK;
+
+namespace osu.Game.Rulesets.BeatFighter.Replays
+{
+    public class BeatFighterReplayFrame : ReplayFrame
+    {
+        public List<BeatFighterAction> Actions = new List<BeatFighterAction>();
+        public Vector2 Position;
+
+        public BeatFighterReplayFrame(BeatFighterAction? button = null)
+        {
+            if (button.HasValue)
+                Actions.Add(button.Value);
+        }
+
+        public override bool IsEquivalentTo(ReplayFrame other)
+            => other is BeatFighterReplayFrame freeformFrame && Time == freeformFrame.Time && Position == freeformFrame.Position && Actions.SequenceEqual(freeformFrame.Actions);
+    }
+}
---
+++ osu.Game.Rulesets.BeatFighter/UI/DrawableBeatFighterRuleset.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Framework.Input;
+using osu.Game.Beatmaps;
+using osu.Game.Input.Handlers;
+using osu.Game.Replays;
+using osu.Game.Rulesets.BeatFighter.Objects;
+using osu.Game.Rulesets.BeatFighter.Objects.Drawables;
+using osu.Game.Rulesets.BeatFighter.Replays;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.UI;
+
+namespace osu.Game.Rulesets.BeatFighter.UI
+{
+    [Cached]
+    public partial class DrawableBeatFighterRuleset : DrawableRuleset<BeatFighterHitObject>
+    {
+        public DrawableBeatFighterRuleset(BeatFighterRuleset ruleset, IBeatmap beatmap, IReadOnlyList<Mod> mods = null)
+            : base(ruleset, beatmap, mods)
+        {
+        }
+
+        protected override Playfield CreatePlayfield() => new BeatFighterPlayfield();
+
+        protected override ReplayInputHandler CreateReplayInputHandler(Replay replay) => new BeatFighterFramedReplayInputHandler(replay);
+
+        public override DrawableHitObject<BeatFighterHitObject> CreateDrawableRepresentation(BeatFighterHitObject h) => new DrawableBeatFighterHitObject(h);
+
+        protected override PassThroughInputManager CreateInputManager() => new BeatFighterInputManager(Ruleset?.RulesetInfo);
+    }
+}
